70b2eb62cf89164dde5914a27f35879f035fdd6f
Stoicheia Magest <stoicheiamagest@wlan-su-10-201-5-69.local.su.se>
2026-05-14 12:06:25 +0200

    BFS allmost done?

--- backend/src/main/java/se/su/inlupp/BFSPathFinder.java
+++ backend/src/main/java/se/su/inlupp/BFSPathFinder.java
@@ -1,10 +1,47 @@
 package se.su.inlupp;
+import java.util.*;
 
 public class BFSPathFinder<T> implements PathFinder<T> {
 
+  public BFSPathFinder() {
+    }
+
   @Override
   public Path<T> findPath(Graph<T> graph, T from, T to) {
-    throw new UnsupportedOperationException("Unimplemented method 'findPath'");
+    Map<T, T> searchOrder = new HashMap<>();
+    searchOrder.put(from, null);
+    LinkedList<T> queue = new LinkedList<>();
+
+    queue.add(from);
+
+    while (!queue.isEmpty()) {
+      T current = queue.poll();
+      for (Edge<T> edge : graph.getEdgesFrom(current)) {
+        T next = edge.getDestination();
+        if (!searchOrder.containsKey(next)) {
+          searchOrder.put(next, current);
+          queue.add(next);
+        }
+      }      
+    }
+
+    LinkedList<Edge<T>> path = new LinkedList<>();
+
+    T current = to;
+    while (current != null && !current.equals(from)) {
+      T next = searchOrder.get(current);
+      Edge<T> edge = graph.getEdgeBetween(next, current);
+      path.addFirst(edge);
+      current = next;
+    }
+
+    List<T> nodes = path.stream()
+                        .map(e -> e.getDestination())
+                        .toList();
+
+    nodes.addFirst(from);
+
+    return new PathClass<T>(path, nodes);    
   }
 }
 
